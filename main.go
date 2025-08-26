867c1abdc5c2e777fc832a6626160efc7457e8ee
Vit <alekseevitaly_1095@mail.ru>
2025-08-26 18:52:04 +0300

    Коммит конвертер валют

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main()  {
-	
+import "fmt"
+
+func main() {
+	const usdToEur = 0.86
+	const usdToRub = 80.45
+
+	//Рассчитать EUR в RUB на основании первых двух переменных-констант
+
+	const eurToUsd = 1 / usdToEur
+	const eurToRub = usdToRub * eurToUsd
+	fmt.Printf("Один евро в рублях составит: %.2f руб.", eurToRub)
+
 }
